b135287ef7f8f31f1ea15a0f2564253ba66f0655
فرزاد محمدی <farzadmohamadi201@gmail.com>
2023-04-25 16:09:27 +0330

    add service and interface for addNewcontact

---
+++ ApplicationPhoneBook/Services/AddNewContact/AddNewContactService.cs
@@ -0,0 +1,36 @@
+﻿using ApplicationPhoneBook.DataBase;
+using ApplicationPhoneBook.Dto;
+using DomainPhoneBook.Entities;
+
+namespace ApplicationPhoneBook.Services.AddNewContact
+{
+    public class AddNewContactService : IAddNewContactService
+    {
+        private readonly IDataBaseContext dataBaseContext;
+
+        public AddNewContactService(IDataBaseContext dataBaseContext)
+        {
+            this.dataBaseContext = dataBaseContext;
+        }
+        public ResultDto Execute(AddNewContactDto NewContact)
+        {
+            if (string.IsNullOrEmpty(NewContact.PhoneNumber))
+            {
+                return new ResultDto
+                {
+                    IsSuccess = false,
+                    Message = "شماره موبایل اجباری می باشد. لطفا شماره موبایل هم وارد کنید."
+                };
+            }
+            Contact contact = new Contact(NewContact.Name, NewContact.LastName, NewContact.PhoneNumber, NewContact.Company);
+            dataBaseContext.Contacts.Add(contact);
+            dataBaseContext.SaveChanges();
+
+            return new ResultDto
+            {
+                IsSuccess = true,
+                Message = $"مخاطب {contact.Name} {contact.LastName} با موفقیت در دیتابیس ذخیره شد.",
+            };
+        }
+    }
+}
