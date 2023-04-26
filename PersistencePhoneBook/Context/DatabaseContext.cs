d63023e45d0e24b3b8bc42f81196edc2678ebc4d
فرزاد محمدی <farzadmohamadi201@gmail.com>
2023-04-26 07:54:15 +0330

    implementation persistence

---
+++ PersistencePhoneBook/Context/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationPhoneBook.DataBase;
+using DomainPhoneBook.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersistencePhoneBook.Context
+{
+    public class DatabaseContext : DbContext, IDataBaseContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-QM4GS45;Initial Catalog=NewDbContact;Integrated Security=True;");
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
